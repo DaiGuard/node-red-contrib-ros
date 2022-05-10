376a8e8ef2e4994fdfffc9c4464cd07737a33d42
DaiGuard <zenith.or.w@gmail.com>
2022-05-11 01:17:22 +0900

    Draft: add action client

--- ros.js
+++ ros.js
@@ -3,11 +3,13 @@ module.exports = function(RED) {
 	var RosTimeNode = require('./ros-time-node')(RED);
 	var RosPublishNode = require('./ros-publish-node')(RED);
 	var RosServiceNode = require('./ros-service-node')(RED);
+	var RosActionNode = require('./ros-action-node')(RED);
 	var RosServerNode = require('./ros-server-node')(RED);	
 
 	RED.nodes.registerType("ros-subscribe",RosSubscribeNode);
 	RED.nodes.registerType("ros-time",RosTimeNode);
 	RED.nodes.registerType("ros-publish",RosPublishNode);
 	RED.nodes.registerType("ros-service",RosServiceNode);
+	RED.nodes.registerType("ros-action",RosActionNode);
 	RED.nodes.registerType("ros-server",RosServerNode);
 }
